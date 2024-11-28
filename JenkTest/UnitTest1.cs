87c64b8d32fdd8d8e99fd02f998e8c2cb28ca603
SacarLite <valden.bokovoi@mail.ru>
2024-11-28 16:07:55 +0300

    Добавьте файлы проекта.

---
+++ JenkTest/UnitTest1.cs
@@ -0,0 +1,26 @@
+namespace JenkTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.IsTrue(true);
+        }
+        [Test]
+        public void Test2()
+        {
+            Assert.IsTrue(true);
+        }
+        [Test]
+        public void Test3()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
